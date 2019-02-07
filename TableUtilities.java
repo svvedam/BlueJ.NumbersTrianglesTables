8cd5348c7e5640bcc34d9c64f52f8d5e8b3c3de3
Sravani Vedam <sravanivedam@zipcoders-MacBook-Pro.local>
2019-02-07 17:19:39 -0500

    Completed NumbersTrianglesTables assignment

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,55 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String sb = "";
+        int product =0;
+        
+        for(int i =1 ; i<=5 ;i++){
+            
+           for(int j=1;j<=5;j++){
+               product= i*j;
+               sb= sb + String.format("%3d |",product);
+            }
+            sb =  sb + "\n";
+        }
+        return sb.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String sb = "";
+        int product =0;
+        
+        for(int i =1 ; i<=10 ;i++){
+            
+           for(int j=1;j<=10;j++){
+               product= i*j;
+               sb= sb + String.format("%3d |",product);
+            }
+            sb =  sb + "\n";
+        }
+        return sb.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+         String sb = "";
+        int product =0;
+        
+        for(int i =1 ; i<=tableSize ;i++){
+            
+           for(int j=1;j<=tableSize;j++){
+               product= i*j;
+               sb= sb + String.format("%3d |",product);
+            }
+            sb =  sb + "\n";
+        }
+        return sb.toString();
     }
+    
+    public static void main(String[] args){
+        String str=null;
+        TableUtilities tbu= new TableUtilities();
+        str=tbu.getSmallMultiplicationTable();
+        
+    }
+
 }
--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,44 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String s = "";
+        for(int i=1; i<=numberOfStars ;i++){
+            s=s+"*";
+        }
+        return s;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String s = "";
+        for(int i=1; i<=numberOfRows ;i++){
+            for(int j=1;j<=i;j++){
+            s=s+"*";
+            }
+            s=s+"\n";
+        }
+        return s;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        String s = "";
+        for(int i=1; i<=4 ;i++){
+            for(int j=1;j<=i;j++){
+            s=s+"*";
+            }
+            s=s+"\n";
+        }
+        return s;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String s = "";
+        for(int i=1; i<=9 ;i++){
+            for(int j=1;j<=i;j++){
+            s=s+"*";
+            }
+            s=s+"\n";
+        }
+        return s;
     }
 }
